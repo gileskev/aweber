7b425b2f0a329b9830fe308783323a73d0fd39b6
Kevin Giles <nivekselig@pm.me>
2023-12-17 17:56:34 -0500

    add form fields to page

--- src/components/passwords.jsx
+++ src/components/passwords.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { validatePasswordLength,
          validatePasswordLowerCase, 
          validatePasswordUpperCase, 
@@ -6,16 +7,52 @@ import { validatePasswordLength,
          validatePasswordMatch } from "../utils/passwordValidation";
 
 const Passwords = () => {
+    const [messages, setMessages] = useState([]);
+    const [passwordValid, setPassordValid] = useState(true);
+
+    const handlePasswordValidation = (password, newpassword) => {
+        // check to see if password matches new password
+        if(!validatePasswordMatch(password, newpassword)) {
+            setMessages([...messages, "Passwords do not match"])
+            setPassordValid(false);
+        }
+
+        // check to see if passwords contain a number
+        if(!validatePasswordNumber(password)) {
+            if(!validatePasswordNumber(password)) {
+                setMessages([...messages, "Password does not contain a number"])
+                setPassordValid(false);
+            }
+        }
+
+        // check to see if password contains lowercase letter
+        if(!validatePasswordLowerCase(password)) {
+            setMessages([...messages, "Password does not contain lowercase letter"])
+            setPassordValid(false);
+        }
+
+        // check to see if password contains uppercase letter
+        if(!validatePasswordUpperCase(password)) {
+            setMessages([...messages, "Password does not contain uppercase letter"])
+            setPassordValid(false);
+        }
+
+        // check to see if password contains special character
+        if(!validatePasswordSpecialCharacter(password)) {
+            setMessages([...messages, "Password does not contain special character"])
+        }
+
+    }
+
+    return(
     <>
-        <label htmlFor="newpassword">
-            New Password:
-            <input id="newpassword" type="password" />
-        </label>
-        <label htmlFor="newPasswordConfirm">
-            Confirm New Password
-            <input id="newPasswordConfirm" type="password" />
-        </label>
+        <label htmlFor="newpassword">New Password:</label>
+        <input id="newpassword" type="password" />
+        <label htmlFor="newPasswordConfirm">Confirm New Password</label>
+        <input id="newPasswordConfirm" type="password" />
+        <input type="submit" value="submit"/>
     </>
+    )
 }
 
 
